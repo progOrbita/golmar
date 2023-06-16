8dce8a0ea3002ce5e1980e488804484835aeb242
progOrbita <web.orbitadigital@gmail.com>
2023-06-16 09:56:06 +0200

    Update GolmarApi.php
    add namespace and imports to class

--- src/GolmarApi.php
+++ src/GolmarApi.php
@@ -1,3 +1,13 @@
 <?php
+
+declare(strict_types=1);
+
+namespace OrbitaDigital\Golmar;
+
+use Symfony\Contracts\Cache\ItemInterface;
+use Symfony\Component\Cache\Adapter\FilesystemAdapter;
+use DOMDocument;
+use DOMXPath;
+
 class GolmarApi
 }
